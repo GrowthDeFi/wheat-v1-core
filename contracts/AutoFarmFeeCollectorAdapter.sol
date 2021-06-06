26738538c1617d3bc5b5e45f831b7fe9868b5f4a
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-06-06 14:51:12 -0300

    Adds NatSpec comments to AutoFarmFeeCollectorAdapter.sol

--- contracts/AutoFarmFeeCollectorAdapter.sol
+++ contracts/AutoFarmFeeCollectorAdapter.sol
@@ -8,16 +8,32 @@ import { WhitelistGuard } from "./WhitelistGuard.sol";
 
 import { Transfers } from "./modules/Transfers.sol";
 
+/**
+ * This contract implements a fee collector adapter to be used with AutoFarm
+ * strategies, it converts the source reward token (AUTO) into the target
+ * reward token (CAKE) whenever the gulp function is called.
+ */
 contract AutoFarmFeeCollectorAdapter is ReentrancyGuard, WhitelistGuard
 {
+	// adapter token configuration
 	address public immutable sourceToken;
 	address public immutable targetToken;
 
+	// addresses receiving tokens
 	address public treasury;
 	address public collector;
 
+	// exchange contract address
 	address public exchange;
 
+	/**
+	 * @dev Constructor for this adapter contract.
+	 * @param _sourceToken The input reward token for this contract, to be converted.
+	 * @param _targetToken The output reward token for this contract, to convert to.
+	 * @param _treasury The treasury address used to recover lost funds.
+	 * @param _collector The fee collector address to send converted funds.
+	 * @param _exchange The exchange contract used to convert funds.
+	 */
 	constructor (address _sourceToken, address _targetToken,
 		address _treasury, address _collector, address _exchange) public
 	{
@@ -29,11 +45,24 @@ contract AutoFarmFeeCollectorAdapter is ReentrancyGuard, WhitelistGuard
 		exchange = _exchange;
 	}
 
+	/**
+	 * @notice Allows for the beforehand calculation of the amount of
+	 *         source reward token to be converted on the next
+	 *         gulp call.
+	 * @return _totalSource The amount of the source reward token to be converted.
+	 */
 	function pendingSource() external view returns (uint256 _totalSource)
 	{
 		return Transfers._getBalance(sourceToken);
 	}
 
+	/**
+	 * @notice Allows for the beforehand calculation of the amount of
+	 *         target reward token to be converted on the next
+	 *         gulp call.
+	 * @return _totalTarget The expected amount of the target reward token
+	 *                      to be sent to collector after conversion.
+	 */
 	function pendingTarget() external view returns (uint256 _totalTarget)
 	{
 		require(exchange != address(0), "exchange not set");
@@ -42,6 +71,12 @@ contract AutoFarmFeeCollectorAdapter is ReentrancyGuard, WhitelistGuard
 		return _totalTarget;
 	}
 
+	/**
+	 * Performs the conversion of the accumulated source reward token into
+	 * the target reward token and sends to the fee collector.
+	 * @param _minTotalTarget The minimum amount expected to be sent to the
+	 *                        fee collector.
+	 */
 	function gulp(uint256 _minTotalTarget) external onlyEOAorWhitelist nonReentrant
 	{
 		require(exchange != address(0), "exchange not set");
@@ -53,6 +88,13 @@ contract AutoFarmFeeCollectorAdapter is ReentrancyGuard, WhitelistGuard
 		Transfers._pushFunds(targetToken, collector, _totalTarget);
 	}
 
+	/**
+	 * @notice Allows the recovery of tokens sent by mistake to this
+	 *         contract, excluding tokens relevant to its operations.
+	 *         The full balance is sent to the treasury address.
+	 *         This is a privileged function.
+	 * @param _token The address of the token to be recovered.
+	 */
 	function recoverLostFunds(address _token) external onlyOwner
 	{
 		require(_token != sourceToken, "invalid token");
@@ -60,14 +102,11 @@ contract AutoFarmFeeCollectorAdapter is ReentrancyGuard, WhitelistGuard
 		Transfers._pushFunds(_token, treasury, _balance);
 	}
 
-	function setCollector(address _newCollector) external onlyOwner
-	{
-		require(_newCollector != address(0), "invalid address");
-		address _oldCollector = collector;
-		collector = _newCollector;
-		emit ChangeCollector(_oldCollector, _newCollector);
-	}
-
+	/**
+	 * @notice Updates the treasury address used to recover lost funds.
+	 *         This is a privileged function.
+	 * @param _newTreasury The new treasury address.
+	 */
 	function setTreasury(address _newTreasury) external onlyOwner
 	{
 		require(_newTreasury != address(0), "invalid address");
@@ -76,6 +115,25 @@ contract AutoFarmFeeCollectorAdapter is ReentrancyGuard, WhitelistGuard
 		emit ChangeTreasury(_oldTreasury, _newTreasury);
 	}
 
+	/**
+	 * @notice Updates the fee collector address used to collect the performance fee.
+	 *         This is a privileged function.
+	 * @param _newCollector The new fee collector address.
+	 */
+	function setCollector(address _newCollector) external onlyOwner
+	{
+		require(_newCollector != address(0), "invalid address");
+		address _oldCollector = collector;
+		collector = _newCollector;
+		emit ChangeCollector(_oldCollector, _newCollector);
+	}
+
+	/**
+	 * @notice Updates the exchange address used to convert funds. A zero
+	 *         address can be used to temporarily pause conversions.
+	 *         This is a privileged function.
+	 * @param _newExchange The new exchange address.
+	 */
 	function setExchange(address _newExchange) external onlyOwner
 	{
 		address _oldExchange = exchange;
@@ -83,6 +141,7 @@ contract AutoFarmFeeCollectorAdapter is ReentrancyGuard, WhitelistGuard
 		emit ChangeExchange(_oldExchange, _newExchange);
 	}
 
+	// events emitted by this contract
 	event ChangeCollector(address _oldCollector, address _newCollector);
 	event ChangeTreasury(address _oldTreasury, address _newTreasury);
 	event ChangeExchange(address _oldExchange, address _newExchange);
