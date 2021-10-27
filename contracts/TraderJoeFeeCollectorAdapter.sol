921dc228133c43cf5d203c8ad34d8e688d495252
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-10-27 11:33:21 -0300

    Fits within contract size restrictions

--- contracts/TraderJoeFeeCollectorAdapter.sol
+++ contracts/TraderJoeFeeCollectorAdapter.sol
@@ -16,7 +16,6 @@ import { Transfers } from "./modules/Transfers.sol";
 		20% AVAX with Banker
 		20% WETH.e with Banker
 		10% WBTC.e with Banker
-	100% of WAVAX harvested would go directly to the AVAX collector.
  */
 contract TraderJoeFeeCollectorAdapter is ReentrancyGuard, /*WhitelistGuard,*/ DelayedActionGuard
 {
@@ -24,9 +23,13 @@ contract TraderJoeFeeCollectorAdapter is ReentrancyGuard, /*WhitelistGuard,*/ De
 
 	// adapter token configuration
 	address public immutable sourceToken;
+	address public immutable bonusToken;
+	address[] public targetTokens;
 
 	// addresses receiving tokens
+	address public psm;
 	address public treasury;
+	mapping (address => address) public collectors;
 
 	// exchange contract address
 	address public exchange;
@@ -34,10 +37,12 @@ contract TraderJoeFeeCollectorAdapter is ReentrancyGuard, /*WhitelistGuard,*/ De
 	// minimal gulp factor
 	uint256 public minimalGulpFactor = DEFAULT_MINIMAL_GULP_FACTOR;
 
-	constructor (address _sourceToken/*, address _targetToken*/,
-		address _treasury/*, address _collector*/, address _exchange) public
+	constructor (address _sourceToken, address _bonusToken,
+		address _psm, address _treasury, address _exchange) public
 	{
 		sourceToken = _sourceToken;
+		bonusToken = _bonusToken;
+		psm = _psm;
 		treasury = _treasury;
 		exchange = _exchange;
 	}
@@ -50,15 +55,20 @@ contract TraderJoeFeeCollectorAdapter is ReentrancyGuard, /*WhitelistGuard,*/ De
 	function _gulp() internal returns (bool _success)
 	{
 		require(exchange != address(0), "exchange not set");
-/*
 		uint256 _totalSource = Transfers._getBalance(sourceToken);
-		uint256 _factor = IExchange(exchange).oracleAveragePriceFactorFromInput(sourceToken, targetToken, _totalSource);
-		if (_factor < minimalGulpFactor) return false;
-		Transfers._approveFunds(sourceToken, exchange, _totalSource);
-		IExchange(exchange).convertFundsFromInput(sourceToken, targetToken, _totalSource, 1);
-		uint256 _totalTarget = Transfers._getBalance(targetToken);
-		Transfers._pushFunds(targetToken, collector, _totalTarget);
-*/
+		for (uint256 _i = 0; _i < targetTokens.length; _i++) {
+			address _targetToken = targetTokens[_i];
+			uint256 _factor = IExchange(exchange).oracleAveragePriceFactorFromInput(sourceToken, _targetToken, _totalSource);
+			if (_factor < minimalGulpFactor) return false;
+			Transfers._approveFunds(sourceToken, exchange, _totalSource);
+			IExchange(exchange).convertFundsFromInput(sourceToken, _targetToken, _totalSource, 1);
+			uint256 _totalTarget = Transfers._getBalance(_targetToken);
+			Transfers._pushFunds(_targetToken, collectors[_targetToken], _totalTarget);
+		}
+		{
+			uint256 _totalBonus = Transfers._getBalance(bonusToken);
+			Transfers._pushFunds(bonusToken, collectors[bonusToken], _totalBonus);
+		}
 		return true;
 	}
 
@@ -92,6 +102,33 @@ contract TraderJoeFeeCollectorAdapter is ReentrancyGuard, /*WhitelistGuard,*/ De
 	}
 
 	/**
+	 * @notice Updates the fee collector address used to collect the performance fee.
+	 *         This is a privileged function.
+	 * @param _newCollector The new fee collector address.
+	 */
+	function setCollector(address _token, address _newCollector) external onlyOwner
+		delayed(this.setCollector.selector, keccak256(abi.encode(_token, _newCollector)))
+	{
+		require(_newCollector != address(0), "invalid address");
+		address _oldCollector = collectors[_token];
+		collectors[_token] = _newCollector;
+		emit ChangeCollector(_token, _oldCollector, _newCollector);
+	}
+
+	/**
+	 * @notice Updates the peg stability module address used to collect
+	 *         lending fees. This is a privileged function.
+	 * @param _newPsm The new peg stability module address.
+	 */
+	function setPsm(address _newPsm) external onlyOwner
+		delayed(this.setPsm.selector, keccak256(abi.encode(_newPsm)))
+	{
+		address _oldPsm = psm;
+		psm = _newPsm;
+		emit ChangePsm(_oldPsm, _newPsm);
+	}
+
+	/**
 	 * @notice Updates the exchange address used to convert funds. A zero
 	 *         address can be used to temporarily pause conversions.
 	 *         This is a privileged function.
@@ -121,7 +158,9 @@ contract TraderJoeFeeCollectorAdapter is ReentrancyGuard, /*WhitelistGuard,*/ De
 		emit ChangeMinimalGulpFactor(_oldMinimalGulpFactor, _newMinimalGulpFactor);
 	}
 
+	event ChangePsm(address _oldPsm, address _newPsm);
 	event ChangeTreasury(address _oldTreasury, address _newTreasury);
+	event ChangeCollector(address indexed _token, address _oldCollector, address _newCollector);
 	event ChangeExchange(address _oldExchange, address _newExchange);
 	event ChangeMinimalGulpFactor(uint256 _oldMinimalGulpFactor, uint256 _newMinimalGulpFactor);
 }
